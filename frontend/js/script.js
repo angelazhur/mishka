b13e3bd317a70e91955bb41d4fd8d2f4438e3f2a
Анжела Журавлева <anzelazuravleva@MacBook-Air-Anzela.local>
2020-04-01 11:20:44 +0500

    second commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    
-    let msg = 'Welcome';
-    console.log(msg + '!');
 
+	let openbtn = $(".mobile-menu__btn");
+	let menu = $(".mobile-menu__dropdown");
+	let close = $(".mobile-menu__close");
+
+	openbtn.on("click", function(e) {
+
+		if(openbtn.hasClass("active")) {
+
+			openbtn.removeClass("active");
+			menu.removeClass("show");
+
+		} else {
+
+			openbtn.addClass("active");
+			menu.addClass("show");
+
+		} // else
+
+	}); // click
 });
